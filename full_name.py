95040b826b4136aa6cf607334f0eff7c4ca1216f
s2pak-denys <s.denys19.m@gmail.com>
2024-03-08 23:29:52 +0200

    gitpractice

---
+++ full_name.py
@@ -0,0 +1,10 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+
+#print(ful_name)
+
+#print(f"Hello, {full_name.title()}!")
+
+print(message)
---
+++ hello_world messag.py
@@ -0,0 +1,7 @@
+messag = "Hallo Python World"
+print(messag)
+
+messag1 = "Hallo Python Crash Course World"
+print(messag1)
+
+print(messag,messag1)
